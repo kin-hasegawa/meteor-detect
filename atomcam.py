4e0226254bd2b8be3a84a6b9bf85685756c4c689
hasegawa <kin3.hasegawa@gmail.com>
2022-01-14 23:50:28 +0900

    入出力用のディレクトリ名を指定できるようにした。 共通関数を外に出してまとめた。

--- atomcam.py
+++ atomcam.py
@@ -2,7 +2,8 @@
 
 from pathlib import Path
 import sys
-import datetime
+import re
+from datetime import datetime
 import time
 import argparse
 import numpy as np
@@ -14,6 +15,48 @@ sys.stdout.reconfigure(line_buffering=True)
 
 
 ATOM_CAM_RTSP = 'rtsp://192.168.2.111:8554/unicast'
+
+
+def composite(list_images):
+    # 画像リストの合成
+    equal_fraction = 1.0 / (len(list_images))
+
+    output = np.zeros_like(list_images[0])
+
+    for img in list_images:
+        output = output + img * equal_fraction
+
+    output = output.astype(np.uint8)
+
+    return output
+
+
+def brightest(img_list):
+    output = img_list[0]
+
+    for img in img_list[1:]:
+        output = np.where(output > img, output, img)
+
+    return output
+
+
+def diff(img_list, mask):
+    diff_list = []
+    for img1, img2 in zip(img_list[:-2], img_list[1:]):
+        img1 = cv2.bitwise_or(img1, mask)
+        img2 = cv2.bitwise_or(img2, mask)
+        diff_list.append(cv2.subtract(img1, img2))
+
+    return diff_list
+
+
+def detect(img):
+    blur_size = (5, 5)
+    blur = cv2.GaussianBlur(img, blur_size, 0)
+    canny = cv2.Canny(blur, 100, 200, 3)
+
+    # The Hough-transform algo:
+    return cv2.HoughLinesP(canny, 1, np.pi/180, 25, minLineLength=30, maxLineGap=5)
 
 
 class AtomCam:
@@ -32,6 +75,7 @@ class AtomCam:
         self.capture.release()
         cv2.destroyAllWindows()
 
+    '''
     def composite(self, list_images):
         # 画像リストの合成
         equal_fraction = 1.0 / (len(list_images))
@@ -69,16 +113,24 @@ class AtomCam:
 
         # The Hough-transform algo:
         return cv2.HoughLinesP(canny, 1, np.pi/180, 25, minLineLength=30, maxLineGap=5)
+    '''
 
-    def streaming(self, exposure, no_window):
+    def streaming(self, exposure, no_window, output):
         '''
         ストリーミング再生
           exposure: 比較明合成する時間(sec)
           no_window: True .. 画面表示しない
+          output: 出力先ディレクトリ
 
           return 0 終了
           return 1 異常終了
         '''
+        if output:
+            output_dir = Path(output)
+            output_dir.mkdir(exist_ok=True)
+        else:
+            output_dir = Path('.')
+
         num_frames = int(self.FPS * exposure)
         composite_img = None
 
@@ -114,21 +166,22 @@ class AtomCam:
                 # 画像のコンポジット(単純スタック)
                 #composite_img = self.composite(img_list)
                 # 画像のコンポジット(比較明合成)
-                composite_img = self.brightest(img_list)
-                diff_img = self.brightest(self.diff(img_list))
+                composite_img = brightest(img_list)
+                diff_img = brightest(diff(img_list, self.mask))
                 try:
                     blur_img = cv2.medianBlur(composite_img, 3)
                     if not no_window:
                         cv2.imshow('ATOM Cam2 x {} frames '.format(number), blur_img)
                         # cv2.imshow('ATOM Cam2 x {} frames '.format(number), composite_img)
-                    if self.detect(diff_img) is not None:
+                    if detect(diff_img) is not None:
                         now = datetime.datetime.now()
                         obs_time = "{:04}/{:02}/{:02} {:02}:{:02}:{:02}".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
                         print('{} A possible meteor was detected.'.format(obs_time))
                         filename = "{:04}{:02}{:02}{:02}{:02}{:02}".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
-                        cv2.imwrite(filename + ".jpg", composite_img)
+                        path_name = str(Path(output_dir, filename + ".jpg"))
+                        cv2.imwrite(path_name, composite_img)
                 except Exception as e:
-                    print(str(e))
+                    print(str(e), file=sys.stderr)
             else:
                 print('No data: communcation lost? or end of data', file=sys.stderr)
                 return 1
@@ -143,51 +196,28 @@ class DetectMeteor():
         self.capture = FileVideoStream(file_path).start()
         self.FPS = 15
         date_element = file_path.split('/')
-        self.date = date_element[0]
-        self.hour = date_element[1]
-        self.minute = date_element[2].split('.')[0]
-        self.obs_time = "{} {}:{}".format(self.date, self.hour, self.minute)
+        self.date_dir = date_element[-3]
+        self.date = datetime.strptime(self.date_dir, "%Y%m%d")
+
+        self.hour = date_element[-2]
+        self.minute = date_element[-1].split('.')[0]
+        self.obs_time = "{}/{:02}/{:02} {}:{}".format(self.date.year, self.date.month, self.date.day, self.hour, self.minute)
 
         # 時刻表示部分のマスクを作成
         zero = np.zeros((1080, 1920, 3), np.uint8)
         self.mask = cv2.rectangle(zero, (1390,1010),(1920,1080),(255,255,255), -1)
 
-    def brightest(self, img_list, gaussian=False):
-        output = img_list[0]
-        if gaussian:
-            output = cv2.GaussianBlur(output, (3,3), 0)
-
-        for img in img_list[1:]:
-            if gaussian:
-                img = cv2.GaussianBlur(img, (3,3), 0)
-            output = np.where(output > img, output, img)
-
-        return output
-
-    def diff(self, img_list):
-        diff_list = []
-        for img1, img2 in zip(img_list[:-2], img_list[1:]):
-            img1 = cv2.bitwise_or(img1, self.mask)
-            img2 = cv2.bitwise_or(img2, self.mask)
-            diff_list.append(cv2.subtract(img1, img2))
-
-        return diff_list
-
-    def detect(self, img):
-        blur_size = (5, 5)
-        blur = cv2.GaussianBlur(img, blur_size, 0)
-        canny = cv2.Canny(blur, 100, 200, 3)
-
-        # The Hough-transform algo:
-        # return cv2.HoughLinesP(canny, 1, np.pi/180, 25, minLineLength=50, maxLineGap=5)
-        return cv2.HoughLinesP(canny, 1, np.pi/180, 25, minLineLength=30, maxLineGap=5)
-
-
-    def meteor(self, sec=1):
+    def meteor(self, exposure=1, output=None):
         '''
         流星の検出
         '''
-        num_frames = int(self.FPS * sec)
+        if output:
+            output_dir = Path(output)
+            output_dir.mkdir(exist_ok=True)
+        else:
+            output_dir = Path('.')
+
+        num_frames = int(self.FPS * exposure)
         composite_img = None
 
         count = 0
@@ -211,58 +241,64 @@ class DetectMeteor():
 
             if number > 2:
                 try:
-                    diff_img = self.brightest(self.diff(img_list))
-                    composite_img = self.brightest(img_list)
+                    diff_img = brightest(diff(img_list, self.mask))
+                    composite_img = brightest(img_list)
                     # blur_img = cv2.medianBlur(composite_img, 3)
-                    if self.detect(diff_img) is not None:
-                        obs_time = "{}:{}".format(self.obs_time, str(count*sec).zfill(2))
+                    if detect(diff_img) is not None:
+                        obs_time = "{}:{}".format(self.obs_time, str(count*exposure).zfill(2))
                         print('{}  A possible meteor was detected.'.format(obs_time))
-                        filename = self.date + self.hour + self.minute + str(count*sec).zfill(2)
-                        #cv2.imwrite(filename + ".jpg", diff_img)
-                        cv2.imwrite(filename + "_b.jpg", composite_img)
-                        
+                        filename = self.date_dir + self.hour + self.minute + str(count*exposure).zfill(2)
+                        path_name = str(Path(output_dir, filename + ".jpg"))
+                        # cv2.imwrite(filename + ".jpg", diff_img)
+                        cv2.imwrite(path_name, composite_img)
                 except Exception as e:
-                    print(str(e))
+                    print(str(e, file=sys.stderr))
             else:
                 return 1
 
 
-def detect_meteor(date, hour=None, minute=None, sec=1):
+# def detect_meteor(date, hour=None, minute=None, sec=1):
+def detect_meteor(args):
     '''
     動画ファイルからの流星の検出
     '''
     print("-----detect meteors-----")
-    data_dir = Path(date)
-    if hour:
-        data_dir = Path(data_dir, hour)
-        if minute:
-            file_path = Path(data_dir, "{}.mp4".format(minute))
+    if args.input:
+        input_dir = Path(args.input)
+    else:
+        input_dir = Path('.')
 
-    if minute:
+    data_dir = Path(input_dir, args.date)
+    if args.hour:
+        data_dir = Path(data_dir, args.hour)
+        if args.minute:
+            file_path = Path(data_dir, "{}.mp4".format(args.minute))
+
+    if args.minute:
         # 単体のmp4ファイルの処理
         print(file_path)
         # atom = AtomCam(str(file_path))
         detecter = DetectMeteor(str(file_path))
-        detecter.meteor(sec)
+        detecter.meteor(args.exposure, args.output)
     else:
         # 1時間内の一括処理
         for file_path in sorted(Path(data_dir).glob("*.mp4")):
             print(Path(file_path))
             detecter = DetectMeteor(str(file_path))
-            detecter.meteor(sec)
+            detecter.meteor(args.exposure)
 
 
-def streaming(url, exposure, no_window):
+def streaming(args):
     print("-----streaming-----")
-    if url:
-        atom = AtomCam(url)
+    if args.url:
+        atom = AtomCam(args.url)
         if not atom.capture.isOpened():
             return
 
     while True:
-        sts = atom.streaming(exposure, no_window)
+        sts = atom.streaming(args.exposure, args.no_window, args.output)
         if sts == 1:
-            if Path(url).suffix == '.mp4':
+            if Path(args.url).suffix == '.mp4':
                 # MP4ファイルの場合は終了する。
                 return
 
@@ -284,9 +320,11 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--date', default=None, help="Date in 'yyyymmdd' format (JST)")
     parser.add_argument('--hour', default=None, help="Hour in 'hh' format (JST)")
     parser.add_argument('-m', '--minute', default=None, help="minute in mm (optional)")
+    parser.add_argument('-i', '--input', default=None, help='検出対象のTOPディレクトリ名')
 
     # 共通オプション
     parser.add_argument('-e', '--exposure', type=int, default=1, help='露出時間(second)')
+    parser.add_argument('-o', '--output', default=None, help='検出画像の出力先ディレクトリ名')
 
     # parser.add_argument('-o', '--output', default=None, help="output directory pathname")
 
@@ -294,7 +332,8 @@ if __name__ == '__main__':
 
     if args.date:
         # 日付がある場合はファイル(ATOMCam形式のファイル)から流星検出
-        detect_meteor(args.date, args.hour, args.minute, args.exposure)
+        # detect_meteor(args.date, args.hour, args.minute, args.exposure)
+        detect_meteor(args)
     else:
         # ストリーミング/動画(MP4)の再生
-        streaming(args.url, args.exposure, args.no_window)
+        streaming(args)
