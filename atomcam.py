032faf2ca6c9d5d8223cd8f2cbc4ebe0880e6b3c
hasegawa <kin3.hasegawa@gmail.com>
2025-12-27 11:00:33 +0900

    minor change

--- atomcam.py
+++ atomcam.py
@@ -10,15 +10,6 @@ import numpy as np
 import cv2
 from imutils.video import FileVideoStream
 import yt_dlp
-'''
-try:
-    import apafy as pafy
-    # apafyはサポートされていないのでpafyにする。
-except Exception:
-    # pafyを使う場合はpacheが必要。
-    import pafy
-'''
-import pafy
 
 # マルチスレッド関係
 import threading
